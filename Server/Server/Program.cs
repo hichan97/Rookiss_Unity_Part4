aa44c138f46cbf3dc25ceff4326ccebffaf78e6b
SeongChan <maeg758@gmail.com>
2025-01-23 16:12:45 +0900

    no message

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace Server
 {
     internal class Program
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ServerCore
+﻿
+namespace ServerCore
 {
     internal class Program
     {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello, Thread!");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+            
+            for(int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) ; }, TaskCreationOptions.LongRunning); //오래걸린다고 명시
+                t.Start();
+            }
+
+            //for (int i = 0; i < 5; i++)
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true); });
+            
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //Thread t = new Thread(MainThread);
+            //t.Start();
+
+            //Console.WriteLine("Hello, World!");
+
+            while (true) 
+            { }
         }
     }
 }
