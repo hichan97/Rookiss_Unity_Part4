3c0a7a81cdba18a9a9f5f403f2ff7a6955e3a873
SeongChan <maeg758@gmail.com>
2025-01-23 17:00:05 +0900

    Volatile bool 이용한 멀티 쓰레드 구현

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,39 +1,35 @@
-﻿
-namespace ServerCore
+﻿namespace ServerCore
 {
-    internal class Program
+    class Program
     {
-        static void MainThread(object state)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Hello, Thread!");
-            }
-        }
+        static bool _stop = false;
 
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!");
+
+            while(_stop == false)
+            {
+                //누군가 stop 신호를 해주기를 기달딘다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
+        }
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
-            
-            for(int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) ; }, TaskCreationOptions.LongRunning); //오래걸린다고 명시
-                t.Start();
-            }
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            //for (int i = 0; i < 5; i++)
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true); });
-            
-            ThreadPool.QueueUserWorkItem(MainThread);
+            Thread.Sleep(1000);
 
-            //Thread t = new Thread(MainThread);
-            //t.Start();
+            _stop = true;
 
-            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("stop 호출");
+            Console.WriteLine("종료 대기중 호출");
 
-            while (true) 
-            { }
+            t.Wait();
+
+            Console.WriteLine("종료 성공");
         }
     }
 }
